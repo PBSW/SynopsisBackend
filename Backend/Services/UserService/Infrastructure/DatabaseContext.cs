ab770b090f9b7f710362bfe3012067db4a53cb6f
Philip Zadeh <PhilEZad@gmail.com>
2024-05-13 10:26:19 +0200

    Made createUser in EF

---
+++ Backend/Services/UserService/Infrastructure/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared;
+
+namespace Infrastructure;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+    
+    public DbSet<User> Users { get; set; }
+}
