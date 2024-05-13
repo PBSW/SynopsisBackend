ff48a506a8f8c6ec9b446bacc6162cf1fde7f98a
Philip Zadeh <PhilEZad@gmail.com>
2024-05-13 14:07:28 +0200

    Made User Delete

--- Backend/Services/UserService/Application/Interface/IUserService.cs
+++ Backend/Services/UserService/Application/Interface/IUserService.cs
@@ -8,4 +8,5 @@ public interface IUserService
     public Task<UserResponse> CreateUserAsync(UserCreate userCreate);
     public Task<List<UserResponse>> GetAllUsersAsync();
     public Task<UserResponse> GetUserByIdAsync(int id);
+    public Task<bool> DeleteUserAsync(int id);
 }
