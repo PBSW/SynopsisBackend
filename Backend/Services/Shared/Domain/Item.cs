c2ea62e95b88c816e0c676b936782561abd97f60
Philip Zadeh <90150224+PhilEZad@users.noreply.github.com>
2024-05-17 05:04:29 +0200

    Added DateTime to domain for Item and List

--- Backend/Services/Shared/Domain/Item.cs
+++ Backend/Services/Shared/Domain/Item.cs
@@ -4,5 +4,7 @@ public class Item
 {
     public int Id { get; set; }
     public string Title { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime DateUpdated { get; set; }
     public bool IsDone { get; set; }
 }
--- Backend/Services/Shared/Domain/ToDoList.cs
+++ Backend/Services/Shared/Domain/ToDoList.cs
@@ -6,5 +6,6 @@ public class ToDoList
     public string Title { get; set; }
     public int UserId { get; set; }
     public DateTime DateCreated { get; set; }
+    public DateTime DateUpdated { get; set; }
     public List<Item> Items { get; set; }
 }
