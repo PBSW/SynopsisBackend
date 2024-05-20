77e3d149831ac1e6c7d4332e0281327726fbcb0c
Philip Zadeh <90150224+PhilEZad@users.noreply.github.com>
2024-05-20 19:55:08 +0200

    Added validators

--- Backend/Services/ItemService/Application/Validators/ItemValidator.cs
+++ Backend/Services/ItemService/Application/Validators/ItemValidator.cs
@@ -7,6 +7,22 @@ public class ItemListValidator : AbstractValidator<List<Item>>
 {
     public ItemListValidator()
     {
+        CascadeMode = CascadeMode.Stop;
+        
+        RuleFor(i => i).NotNull().WithMessage("Item list cannot be null");
+        RuleForEach(i => i).NotNull().WithMessage("Item in list cannot be null");
+        RuleForEach(i => i).SetValidator(new ItemValidator());
     }
 }
 
+public class ItemValidator : AbstractValidator<Item>
+{
+    public ItemValidator()
+    {
+        CascadeMode = CascadeMode.Stop;
+        
+        RuleFor(i => i).NotNull().WithMessage("Item cannot be null");
+        RuleFor(i => i.DateCreated).NotNull().WithMessage("Item date created cannot be null");
+        RuleFor(i => i.ToDoListId).GreaterThan(0).WithMessage("Item ToDoListId must be greater than 0");
+    }
+}
